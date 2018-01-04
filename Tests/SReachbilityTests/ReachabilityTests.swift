5700e3189882093bbea2e8bd20b4a923c13df582
Shial <shial@bitbucket.org>
2018-01-04 18:44:35 +1100

    Update Reachability name

---
+++ Tests/SReachbilityTests/ReachabilityTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SReachbility
+
+class SReachbilityTests: XCTestCase {
+    func testExample() {}
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
